8493da136c55752c677e6ddd64a271b87c231e4c
soninaren <nasoni@microsoft.com>
2019-11-22 17:15:00 -0800

    updating bundle version to 1.1.1

--- build/Settings.cs
+++ build/Settings.cs
@@ -52,7 +52,7 @@ namespace Build
 
         public static string ExtensionBundleVersionRange = "[1.*, 2.0.0)";
 
-        public static string ExtensionBundleBuildVersion = "1.1.0";
+        public static string ExtensionBundleBuildVersion = "1.1.1";
 
         public static string TemplatesVersion = "2.0.10393";
 
