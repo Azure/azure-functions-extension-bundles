fba76dcdcb5e80220d490d61748530f562625a89
soninaren <nasoni@microsoft.com>
2020-04-09 17:53:43 -0700

    cleaning up build steps, fixing the skip logic

--- build/BuildConfiguration.cs
+++ build/BuildConfiguration.cs
@@ -8,14 +8,24 @@ namespace Build
 {
     public class BuildConfiguration
     {
-        public string ConfigurationName { get; set; }
+        public string ConfigurationName { get; set; } = string.Empty;
 
         public string ProjectFileName { get; set; }
 
-        public List<string> RuntimeIdentifiers { get; set; }
+        public string RuntimeIdentifier { get; set; } = string.Empty;
 
         public bool PublishReadyToRun { get; set; }
 
         public OSPlatform OSPlatform { get; internal set; }
+
+        public string ConfigId => $"{ConfigurationName}_{RuntimeIdentifier}";
+
+        public string GeneratedBundleZipFileName => $"{Settings.ExtensionBundleId}.{Settings.ExtensionBundleBuildVersion}_{ConfigId}.zip";
+
+        public string GeneratedBundleZipFilePath => Path.Combine(Settings.ArtifactsDirectory, GeneratedBundleZipFileName);
+
+        public string PublishDirectoryPath => Path.Combine(Settings.RootBinDirectory, $"BundleProject_buildOutput_{ConfigId}");
+
+        public string PublishBinariesPath => Path.Combine(PublishDirectoryPath, "bin");
     }
 }
