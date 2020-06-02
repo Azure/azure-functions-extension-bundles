7f22e2a5391acd2d704576a9e7d984188a73b0ee
soninaren <nasoni@microsoft.com>
2020-06-01 23:37:37 -0700

    Updating the bundle artifacts to match runtime

---
+++ build/BundlePackageConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using static Build.Settings;
+
+namespace Build
+{
+    public class BundlePackageConfiguration
+    {
+        public string PackageIdentifier { get; set; }
+
+        public List<ConfigId> ConfigBinariesToInclude { get; set; } = new List<ConfigId>();
+
+        public string BundleName => $"{ExtensionBundleId}.{ExtensionBundleBuildVersion}_{PackageIdentifier}".Trim('_');
+
+        public string GeneratedBundleZipFileName => $"{BundleName}.zip";
+
+        public string GeneratedBundleZipFilePath => Path.Combine(ArtifactsDirectory, GeneratedBundleZipFileName);
+
+        public string CsProjFilePath { get; set; }
+    }
+
+}
--- build/Program.cs
+++ build/Program.cs
@@ -13,13 +13,15 @@ namespace Build
                 .CreateForTarget(args)
                 .Then(Clean)
                 .Then(DownloadTemplates)
+                .Then(BuildBundleBinariesForWindows)
+                .Then(BuildBundleBinariesForLinux)
                 .Then(GenerateNetCoreV2Bundle)
                 .Then(GenerateNetCoreV3BundlesWindows)
                 .Then(GenerateNetCoreV3BundlesLinux)
-                .Then(CreateRUPackage)
-                .Then(CreateCDNStoragePackage)
-                .Then(CreateCDNStoragePackageWindows)
-                .Then(CreateCDNStoragePackageLinux)
+                 .Then(CreateRUPackage)
+                 .Then(CreateCDNStoragePackage)
+                 .Then(CreateCDNStoragePackageWindows)
+                 .Then(CreateCDNStoragePackageLinux)
                 .Run();
         }
     }
