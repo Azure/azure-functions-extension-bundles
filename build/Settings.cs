b6c77c81d4fa5f8cc2f19c38cfa53533cec547de
yojagad <yojagad@microsoft.com>
2020-07-28 18:50:48 -0700

    Updated template version and ExtensionBundleVersion

--- build/Settings.cs
+++ build/Settings.cs
@@ -52,9 +52,9 @@ namespace Build
 
         public static string ExtensionBundleVersionRange = "[2.*, 3.0.0)";
 
-        public static string ExtensionBundleBuildVersion = "2.0.0";
+        public static string ExtensionBundleBuildVersion = "2.0.1";
 
-        public static string TemplatesVersion = "2.0.1563";
+        public static string TemplatesVersion = "2.0.1573";
 
         public static readonly string RUPackagePath = Path.Combine(RootBinDirectory, $"{ExtensionBundleId}.{ExtensionBundleBuildVersion}_RU_package", ExtensionBundleBuildVersion);
 
