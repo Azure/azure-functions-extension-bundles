3e3bdb3ad3fde05f8581fbb4615534fcf89cab52
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-25 16:09:48 -0700

    Update TemplatesVersion to 2.0.1611

--- build/Settings.cs
+++ build/Settings.cs
@@ -54,7 +54,7 @@ namespace Build
 
         public static string ExtensionBundleBuildVersion = "2.0.1";
 
-        public static string TemplatesVersion = "2.0.1579";
+        public static string TemplatesVersion = "2.0.1611";
 
         public static readonly string RUPackagePath = Path.Combine(RootBinDirectory, $"{ExtensionBundleId}.{ExtensionBundleBuildVersion}_RU_package", ExtensionBundleBuildVersion);
 
