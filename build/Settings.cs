f3743b31f4e5df235956d09a93d63469d3a42eae
Katy Shimizu <kashimiz@microsoft.com>
2021-02-26 14:53:32 -0800

    Updated ExtensionBundleBuildVersion to 2.6.0

--- build/Settings.cs
+++ build/Settings.cs
@@ -52,7 +52,7 @@ namespace Build
 
         public static string ExtensionBundleVersionRange = "[2.*, 3.0.0)";
 
-        public static string ExtensionBundleBuildVersion = "2.5.0";
+        public static string ExtensionBundleBuildVersion = "2.6.0";
 
         public static string TemplatesVersion = "2.0.1701";
 
