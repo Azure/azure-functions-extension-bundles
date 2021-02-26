076ae02ff1fd70cb11242c3b92ced064c84f9ef1
Katy Shimizu <kashimiz@microsoft.com>
2021-02-26 14:52:37 -0800

    Updated ExtensionBundleBuildVersion to 1.8.0

--- build/Settings.cs
+++ build/Settings.cs
@@ -52,7 +52,7 @@ namespace Build
 
         public static string ExtensionBundleVersionRange = "[1.*, 2.0.0)";
 
-        public static string ExtensionBundleBuildVersion = "1.7.0";
+        public static string ExtensionBundleBuildVersion = "1.8.0";
 
         public static string TemplatesVersion = "1.0.1701";
 
