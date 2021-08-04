a9a8595dd9ad5758e534cfea5f672c2305c9b449
soninaren <nasoni@microsoft.com>
2021-08-04 07:42:11 -0700

    Updating build to use latest extension version, adding nuget.config

---
+++ build/BundleConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Build
+{
+    public sealed class BundleConfiguration
+    {
+        private static BundleConfiguration instance = null;
+
+        public static BundleConfiguration Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var configFileContent = FileUtility.ReadAllText(Settings.BundleConfigJsonFilePath);
+                    instance = JsonConvert.DeserializeObject<BundleConfiguration>(configFileContent);
+                }
+                return instance;
+            }
+        }
+
+        [JsonProperty("bundleId")]
+        public string ExtensionBundleId { get; private set; }
+
+        [JsonProperty("bundleVersion")]
+        public string ExtensionBundleVersion { get; private set; }
+
+        [JsonProperty("templateVersion")]
+        public string TemplateVersion { get; private set; }
+    }
+}
--- build/Extension.cs
+++ build/Extension.cs
@@ -16,6 +16,9 @@ namespace Build
         [JsonProperty(PropertyName = "version")]
         public string Version { get; set; }
 
+        [JsonProperty(PropertyName = "majorVersion")]
+        public int MajorVersion { get; set; }
+
         [JsonProperty(PropertyName = "bindings")]
         public List<string> Bindings { get; set; }
     }
