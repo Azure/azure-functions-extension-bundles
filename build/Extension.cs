db5a4ab70e5defe2380fc887da798073fdb5ed38
Chris Gillum <cgillum@microsoft.com>
2022-06-28 10:36:32 -0700

    Fix build break caused by extension name mismatch

--- build/Extension.cs
+++ build/Extension.cs
@@ -1,7 +1,5 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Build
 {
@@ -21,5 +19,10 @@ namespace Build
 
         [JsonProperty(PropertyName = "bindings")]
         public List<string> Bindings { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, v{Version ?? MajorVersion.ToString()}";
+        }
     }
 }
