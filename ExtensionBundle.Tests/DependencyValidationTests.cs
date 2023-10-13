f87c39c82a169e73f13bd3b707196e8b81ad580c
Surgupta <surgupta@microsoft.com>
2023-10-13 11:01:29 -0700

    Added test project to run via pipeline

---
+++ ExtensionBundle.Tests/DependencyValidationTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.Azure.Functions.ExtensionBundle.Tests
+{
+    public class DependencyValidationTests
+    {
+        public DependencyValidationTests()
+        {}
+
+        [Fact]
+        public void Test_Success()
+        {
+            Assert.True(1 == 1);
+        }
+
+        [Fact]
+        public void Test_Fails()
+        {
+            Assert.False(1 == 1);
+        }
+    }
+}
