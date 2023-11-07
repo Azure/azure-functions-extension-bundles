99b8422d002f3fd794b92672bd09282431d3929a
Surgupta <surgupta@microsoft.com>
2023-11-07 11:28:47 -0800

    [v4.x-preview] Added test to validate that bundles updates do not move dependencies outside of safe ranges (#318)

---
+++ build/ScriptRuntimeAssembly.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Build
+{
+    internal sealed class ScriptRuntimeAssembly
+    {
+        public string Name { get; set; }
+
+        public string ResolutionPolicy { get; set; }
+    }
+}
