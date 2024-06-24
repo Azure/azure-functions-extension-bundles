413239b9d252393299742016b9eb3f9b901bae15
Naren Soni <nasoni@microsoft.com>
2024-06-24 15:45:32 -0700

    updating the bundle to build on net6 instead of net3

--- build/BuildSteps.cs
+++ build/BuildSteps.cs
@@ -107,11 +107,6 @@ namespace Build
         public static void BuildBundleBinariesForWindows()
         {
             Settings.WindowsBuildConfigurations.ForEach((config) => BuildExtensionsBundle(config));
-
-            // temporary fix for missing extensions.json
-            string sourceExtensionsJsonPath = Path.Combine(Settings.RootBinDirectory, @"NetCoreApp3_win_x64\bin_v3\win-x64\extensions.json");
-            string extensionsJsonPath = Path.Combine(Settings.RootBinDirectory, @"NetCoreApp3_win_x86\bin_v3\win-x86\extensions.json");
-            File.Copy(sourceExtensionsJsonPath, extensionsJsonPath, true);
         }
 
         public static void BuildBundleBinariesForLinux()
@@ -207,15 +202,6 @@ namespace Build
                 Directory.Move(Path.Combine(buildConfig.PublishDirectoryPath, "bin"), buildConfig.PublishBinDirectoryPath);
             }
 
-            // Temporary fix to copy assembly needed for cosmosDb extension
-            var additionalAssembliesPath = Path.Combine(Directory.GetParent(projectFilePath).FullName, "bin", "Release", "netcoreapp3.1", buildConfig.RuntimeIdentifier == "any" ? String.Empty : buildConfig.RuntimeIdentifier);
-
-            var configAssembly = "System.Configuration.ConfigurationManager.dll";
-                File.Copy(Path.Combine(additionalAssembliesPath, configAssembly), Path.Combine(buildConfig.PublishBinDirectoryPath, configAssembly));
-
-            var permissionsAssembly = "System.Security.Permissions.dll";
-                File.Copy(Path.Combine(additionalAssembliesPath, permissionsAssembly), Path.Combine(buildConfig.PublishBinDirectoryPath, permissionsAssembly));
-
         }
 
         public static void AddBindingInfoToExtensionsJson(string extensionsJson)
@@ -307,7 +293,6 @@ namespace Build
 
         public static void PackageNetCoreV3BundlesLinux()
         {
-            CreateExtensionBundle(Settings.BundlePackageNetCoreV3Any);
             CreateExtensionBundle(Settings.BundlePackageNetCoreV3Linux);
         }
 
