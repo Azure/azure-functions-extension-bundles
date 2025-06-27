d08fefd545efd80b30f174aacf529cbadbfc6961
Susanta Biswas <susbiswas@microsoft.com>
2025-06-27 14:37:21 +0530

    mysql trigger emulation tests added

--- tests/emulator_tests/test_mysql_functions.py
+++ tests/emulator_tests/test_mysql_functions.py
@@ -1,100 +1,81 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-import mysql.connector
 from mysql.connector import Error
 import time
-
 from requests import JSONDecodeError
 from tests.utils import testutils
-
+from tests.emulator_tests.mysql_functions.mysql_test_utils import PersonDAO
 
 class TestMySqlFunctions(testutils.WebHostTestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.person_dao = PersonDAO()
+
 
     @classmethod
     def get_script_dir(cls):
         return testutils.EMULATOR_TESTS_FOLDER / 'mysql_functions'
 
-    def connect_and_insert(self, host='localhost', port=3307, database='testdb', user='user', password='password'):
-        connection = None
-        cursor = None
+    def setUp(self):
         try:
-            # Connection string details
-            connection = mysql.connector.connect(
-                host=host,
-                port=port,
-                database=database,
-                user=user,
-                password=password,
-                autocommit=True,
-                consume_results=True
-            )
-            
-            print('Conn host:', connection.server_host)
-            print("Successfully connected to MySQL database")
-            
-            cursor = connection.cursor(buffered=True)
-            
-            # # Create table if it doesn't exist
-            # create_table_query = """
-            # CREATE TABLE `person` (
-            #     `id` int NOT NULL AUTO_INCREMENT,
-            #     `name` varchar(100) NOT NULL,
-            #     `address` varchar(255) NOT NULL,
-            #     PRIMARY KEY (`id`)
-            #     ) ENGINE=InnoDB;
-            # """
-            # cursor.execute(create_table_query)
-            # print("Table 'person' created or already exists")
-            
-            # alter_table_query = """
-            # ALTER TABLE person
-            # ADD az_func_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE 
-            # CURRENT_TIMESTAMP;
-            # """
-            # cursor.execute(alter_table_query)
-            # print("Table 'person' altered to add 'az_func_updated_at' column")
-
-            # Insert data
-            insert_query = "INSERT INTO `person` (`name`, `address`) VALUES (%s, %s)"
-            insert_data = ('itaque', '0022 Gladys Spring Suite 961\nNasirstad, WI 40333')
-            
-            cursor.execute(insert_query, insert_data)
-            cursor.fetchall()  # Consume any results
-            
-            print(f"Record inserted successfully. Row ID: {cursor.lastrowid}")
-            
-            # Verify the insert
-            cursor.execute("SELECT * FROM person WHERE name = 'itaque'")
-            result = cursor.fetchone()
-            if result:
-                print(f"Inserted record: Name={result[0]}, Address={result[1]}")
-                return result
-                
+            self.person_dao.connect()
+            self.person_dao.create_table()
         except Error as e:
-            print(f"Error while connecting to MySQL: {e}")
+            print(f"[ERROR] Error while setting up Mysql table: {e}")
             raise
+
+    @testutils.retryable_test(3, 5)
+    def test_mysql_trigger(self):
+        """Test MySQL trigger functionality
         
-        finally:
-            if cursor:
-                cursor.close()
-            if connection and connection.is_connected():
-                connection.close()
-                print("MySQL connection is closed")
+        Test Scenario:
+        1. Insert a new person into the database.
+        2. Verify that the insert was successful.
+        3. Azure Mysql trigger function is expected to fire and update the address column with string
+            "Function Triggered".
+        4. Check that the MySQL trigger has fired by verifying the address column's value.
+        """
+        # Insert data to trigger the MySQL trigger
+        row_id = self.person_dao.insert_person('itaque', '0022 Gladys Spring Suite 961\nNasirstad, WI 40333')
+        
+        # Verify the insert was successful
+        self.assertIsNotNone(row_id)
+        
+        # fetch the inserted row to ensure it exists
+        person = self.person_dao.get_person_by_id(row_id)
+        self.assertIsNotNone(person)
+        self.assertEqual(person[0], row_id)
 
-    def test_mysql_insert(self, host='localhost', port=3307, database='testdb', user='user', password='password'):
-        """Test MySQL connection and data insertion"""
-        result = self.connect_and_insert(host, port, database, user, password)
-        result2 = self.webhost.request('POST', 'put_mysql_trigger')
+        print(f"Inserted person: ID={person[0]}, Name={person[1]}, Address={person[2]}")
 
-        print('**** Result ****', result)
+        # MySQL trigger may be processed after some delay
+        # We check it every 2 seconds to allow the trigger to be fired
+        max_retries = 3
+        for try_no in range(max_retries):
+            time.sleep(2)
+            
+            try:
+                # Check that the trigger has fired
+                # If the trigger function is fired, it will update the address column
+                result = self.person_dao.get_person_by_id(row_id)
+                self.assertEqual(result[2], "Function Triggered")
+                
+                break
+            except (AssertionError, JSONDecodeError):
+                if try_no == max_retries - 1:
+                    raise
 
-        print('**** Result 2****', result2)
-        # Assertions to verify the test
+    def test_mysql_insert_connectivity(self):
+        """Test MySQL self.connection and data insertion"""
+        row_id = self.person_dao.insert_person('itaque', '0022 Gladys Spring Suite 961\nNasirstad, WI 40333')
+        result = self.person_dao.get_person_by_id(row_id)
+
         self.assertIsNotNone(result)
-        self.assertEqual(result[1], 'itaque')
-        self.assertEqual(result[2], '0022 Gladys Spring Suite 961\nNasirstad, WI 40333')
+        self.assertEqual(result[0], row_id)
 
 
 if __name__ == "__main__":
     test_instance = TestMySqlFunctions()
-    test_instance.test_mysql_insert(host='ams-mysql8.mysql.database.azure.com', port=3306, database='ams', user='cloudsa', password='Yukoon900')
+    test_instance.setUp()
+    test_instance.test_mysql_insert_connectivity()
+    test_instance.test_mysql_trigger()
